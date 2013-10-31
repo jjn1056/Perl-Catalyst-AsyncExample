5f0bf6e56844593cf91087a1b5d58fe503c97526
John Napiorkowski <jjnapiork@cpan.org>
2013-10-31 09:37:25 -0500

    basic streaming

--- AsyncExample/lib/AsyncExample/Controller/IOAsync/Stream.pm
+++ AsyncExample/lib/AsyncExample/Controller/IOAsync/Stream.pm
@@ -12,25 +12,26 @@ sub start : ChainedParent
 sub stream :Chained('start') Args(0) {
   my ($self, $c) = @_;
   my $wfh = $c->res->write_fh;
-  my $loop = $c->req->env->{'io.async.loop'};
-  my $path = $c->path_to('root','file.png');
+  my $path = $c->path_to('root','lorem.txt');
   $c->res->content_type('image/png');
 
   aio_open "$path", IO::AIO::O_RDONLY, 0, sub {
     my ($fh) = @_ or
       die "$path: $!";
-    $self->read_chunk($fh, $wfh);
+    $self->read_chunk($fh, $wfh, 0);
   }
 }
 
 sub read_chunk {
-  my ($self, $fh, $wfh) = @_;
-  aio_read $fh, 4096, sub {
-    my ($data) = @_ or
-      die "read from fh: $!";
-    if(length $data) {
-      $wfh->write($data);
-      $self->read_chunk($fh, $wfh);
+  my ($self, $fh, $wfh, $offset) = @_;
+  my $buffer = '';
+  aio_read $fh, $offset, 4096, $buffer, 0, sub {
+    my $status = shift;
+    die "read error[$status]: $!" unless $status >= 0;
+    if($status) {
+      $wfh->write($buffer);
+      #AsyncExample->log->warn('did a chunk');
+      $self->read_chunk($fh, $wfh, ($offset + 4096) );
     } else {
       $wfh->close;
       aio_close $fh, sub { };
